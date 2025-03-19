063d328a3fa073e8cb06a612fbdf271b24ec4bcb
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-03-19 12:48:04 +0000

    user login using jwt

--- app/Http/Controllers/JwtAuthController.php
+++ app/Http/Controllers/JwtAuthController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWT;
 
 class JwtAuthController extends Controller
 {
@@ -29,5 +32,35 @@ class JwtAuthController extends Controller
             'user'=>$user,
             'token'=>$token
         ]);
+    }
+
+
+    public function login(Request $request){
+
+        $credentials = $request->only('email','password');
+
+        try{
+            if(! $token = JWTAuth::attempt($credentials)){
+                return response()->json(
+                    [
+                        'error'=>'invalid credentials'
+                    ]
+                    );
+            }
+        }catch(JWTException $e){
+
+            return response()->json(
+                [
+                    'error'=>'invalid token'.$e
+                ]
+                );
+        }
+
+        return response()->json(
+            [
+                'user'=>JWTAuth::user(),
+                'token'=>$token
+            ]
+            );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('register',[JwtAuthController::class ,'register']);
+Route::post('login',[JwtAuthController::class ,'login']);
